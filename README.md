d4ddd80462ceefbd839cb6fcce023f3464759a95
Marcelo Baptista <marcelobaptista@outlook.com>
2024-03-07 22:29:36 -0300

    update to 8.2.2, improvements

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
-## Script para instalação do Cisco Packet Tracer 8.2.1
+## Script para instalação do Cisco Packet Tracer 8.2.2
 
-##### O script foi testado no Manjaro Linux, Arch Linux e Fedora, todos na versão 64 bits
+##### O script foi testado nas distros Manjaro Linux, Arch Linux e Fedora, todos na versão 64 bits
+
+#### Requisitos do sistema:
+
+- CPU AMD64 (x86-64), 4 GB de RAM livre, 1,4 GB de espaço livre em disco
 
 Para instalação automática, execute no terminal:
+
 ```sh
 curl -s https://raw.githubusercontent.com/marcelobaptista/packettracer/master/install_pt.sh | sudo bash
 ```
 ***************************************************************************************************
 
-## Script for installing Cisco Packet Tracer 8.2.1
+## Script for installing Cisco Packet Tracer 8.2.2  
 
-##### The script was tested on Manjaro Linux, Arch Linux and Fedora, all in the 64-bit version
+##### The script was tested on distribution Manjaro Linux, Arch Linux and Fedora, all in the 64-bit version
+
+#### System Requirements:
+
+- AMD64 (x86-64) CPU, 4 GB of free RAM, 1.4 GB of free disk space 
 
 For automatic installation, run in the terminal:
 ```sh
@@ -19,11 +28,10 @@ curl -s https://raw.githubusercontent.com/marcelobaptista/packettracer/master/in
 ***************************************************************************************************
 ## Caso queira baixar o pacote .deb para Ubuntu (20.04, 22.04) LTS siga o link:
 
-https://archive.org/download/cisco-packet-tracer-821-ubuntu-64bit_202304/Cisco_Packet_Tracer_821_Ubuntu_64bit.deb
+https://archive.org/download/packet-tracer-822-amd-64/Packet_Tracer822_amd64_signed.deb
 
 ***************************************************************************************************
 
 ## If you want to download the .deb package for Ubuntu (20.04, 22.04) LTS follow the link:
 
-https://archive.org/download/cisco-packet-tracer-821-ubuntu-64bit_202304/Cisco_Packet_Tracer_821_Ubuntu_64bit.deb
-
+https://archive.org/download/packet-tracer-822-amd-64/Packet_Tracer822_amd64_signed.deb
--- install_pt.sh
+++ install_pt.sh
@@ -5,93 +5,93 @@ if [ $UID -ne 0 ]; then
 	echo "Requer privilégio de root. Execute o script com 'sudo' "
 	exit 1
 fi
-#
-#Diretório temporário usado na instalação
-#
-DIR="/tmp/PacketTracer/"
-#
-#Checa se já existe o diretório /tmp/PacketTracer/ e acessa o mesmo
-#
-if [ -d "$DIR" ]; then
-	rm -rf "$DIR"
-else
-	mkdir /tmp/PacketTracer/
+
+# Diretório temporário usado na instalação
+temp_dir="/tmp/PacketTracer/"
+arquivo="Packet_Tracer822_amd64_signed.deb"
+url="https://archive.org/download/packet-tracer-822-amd-64"
+
+# Checa se já existe o diretório /tmp/PacketTracer/ e acessa o mesmo
+if [ -d "${temp_dir}" ]; then
+	rm -rf "${temp_dir}"
 fi
-cd /tmp/PacketTracer/
-#
-#Baixando e desempacotando o arquivo .deb
-#
-echo ""
-echo "Fazendo o download do arquivo Cisco_Packet_Tracer_821_Ubuntu_64bit.deb..."
-echo "" && curl --progress-bar \
-	-OL "https://archive.org/download/cisco-packet-tracer-821-ubuntu-64bit_202304/Cisco_Packet_Tracer_821_Ubuntu_64bit.deb"
-#
-#Descompacta o pacote Cisco_Packet_Tracer_821_Ubuntu_64bit.deb e demais arquivos para a instalação
-#
-ar -xv Cisco_Packet_Tracer_821_Ubuntu_64bit.deb
+
+# Cria o diretório temporário
+mkdir -p "${temp_dir}"
+cd "${temp_dir}" || exit 1
+
+# Trata os erros de download
+trap 'rm -rf "${temp_dir}"' ERR
+
+# Baixa o arquivo .deb do Cisco Packet Tracer
+printf "\nFazendo o download do arquivo %s...\n" "${arquivo}"
+curl --progress-bar -OL "${url}/${arquivo}" || {
+	echo "Erro ao baixar o arquivo ${arquivo}!"
+	exit 1
+}
+
+# Extrai os arquivos do .deb
+printf "\nExtraindo os arquivos...\n"
+ar -xv "${arquivo}" > /dev/null 2>&1
 mkdir data && tar -C data -Jxf data.tar.xz
-cd data
-#
-#Remove a instalação anterior do PacketTracer (geralmente instalado em /opt/pt)
-#
-xdg-desktop-menu uninstall /usr/share/applications/cisco-*.desktop && update-mime-database /usr/share/mime
-gtk-update-icon-cache --force /usr/share/icons/gnome
-rm -rf /opt/pt && rm -f /usr/local/bin/packettracer
-#
-#Copia os arquivos do PacketTracer 8.2.1
-#
-yes | cp -r usr /
-yes | cp -r opt /
-#
-#Criação de atalhos
-#
-cat <<EOF | tee /usr/share/applications/cisco-pt821.desktop
+cd data || exit 1
+
+# Remove a instalação anterior do PacketTracer (normalmente instalado em /opt/pt)
+xdg-desktop-menu uninstall /usr/share/applications/cisco-*.desktop &&
+	update-mime-database /usr/share/mime
+gtk-update-icon-cache --force /usr/share/icons/* > /dev/null 2>&1
+rm -rf /opt/pt &&
+	rm -f /usr/local/bin/packettracer
+
+# Copia os arquivos do PacketTracer 8.2.2
+printf "\nCopiando os arquivos...\n"
+cp -rf usr opt /
+
+# Criação de atalhos
+printf "\nCriando atalhos...\n"
+cat <<EOF > /usr/share/applications/cisco-pt822.desktop
 [Desktop Entry]
 Type=Application
 Exec=/opt/pt/packettracer %f
-Name=Packet Tracer 8.2.1
+Name=Packet Tracer 8.2.2
 Icon=/opt/pt/art/app.png
 Terminal=false
 StartupNotify=true
 MimeType=application/x-pkt;application/x-pka;application/x-pkz;application/x-pks;application/x-pksz;
 EOF
-#
-cat <<EOF | tee /usr/share/applications/cisco-ptsa821.desktop
+
+cat <<EOF > /usr/share/applications/cisco-ptsa822.desktop
 [Desktop Entry]
 Type=Application
 Exec=/opt/pt/packettracer -uri=%u
-Name=Packet Tracer 8.2.1
+Name=Packet Tracer 8.2.2
 Icon=/opt/pt/art/app.png
 Terminal=false
 StartupNotify=true
 NoDisplay=true
 MimeType=x-scheme-handler/pttp;
 EOF
-#
-#Atualiza o ícone e a associação de arquivos
-#
-xdg-desktop-menu install /usr/share/applications/cisco-pt821.desktop
-xdg-desktop-menu install /usr/share/applications/cisco-ptsa821.desktop
+
+# Atualiza o ícone e a associação de arquivos
+printf "\nAtualizando ícones e a associação de arquivos...\n"
+xdg-desktop-menu install /usr/share/applications/cisco-pt822.desktop
+xdg-desktop-menu install /usr/share/applications/cisco-ptsa822.desktop
 update-mime-database /usr/share/mime
-gtk-update-icon-cache --force /usr/share/icons/gnome
+gtk-update-icon-cache --force /usr/share/icons/* > /dev/null 2>&1
 xdg-mime default cisco-ptsa821.desktop x-scheme-handler/pttp
-#
-#Link simbólico para PacketTracer
-#
+
+# Link simbólico para PacketTracer
+printf "\nCriando link simbólico para PacketTracer...\n"
 ln -sf /opt/pt/packettracer /usr/local/bin/packettracer
-#
-#Corrige permissões
-#
-chown root:root /opt/pt/bin/updatepttp && chmod 4755 /opt/pt/bin/updatepttp
-#
-#Remove arquivos utilizados durante a instalação
-#
-cd .. && rm -rf /tmp/PacketTracer
-#
-#Sai do script
-echo ""
-echo "Instalado com sucesso!!!"
-echo ""
-echo "Bye!Bye!"
-echo ""
-echo ""
+
+# Corrige permissões
+printf "\nCorrigindo permissões...\n"
+chown root:root /opt/pt/bin/updatepttp &&
+	chmod 4755 /opt/pt/bin/updatepttp
+
+# Remove arquivos utilizados durante a instalação
+printf "\nRemovendo arquivos temporários...\n"
+rm -rf "${temp_dir}"
+
+# Sai do script
+printf "\nInstalação concluída com sucesso!\n"
